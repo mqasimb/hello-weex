960195eb9cc4895d33fb1abebb8bf0c6e545333a
Qasim Bhatti <bqasim381@gmail.com>
2017-11-04 01:35:44 -0400

    implement barcode scanner with quaggajs

---
+++ src/components/quagga.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="wrapper">
+    <text>Barcode Scanner - ean & upc</text>
+    <text v-if="results.length > 2">Barcode: {{ results[results.length-1].code }} Format: {{ results[results.length-1].format }}</text>
+    <text v-if="error.length > 0">Errors: {{ error }}</text>
+    <div id="videoElement"></div>
+  </div>
+</template>
+
+<style scoped>
+/*Coming soon*/
+</style>
+
+<script>
+import Quagga from 'quagga'
+  export default {
+    data: function() {
+      return {
+        results: [],
+        error: ''
+      }
+    },
+    mounted: function() {
+      var resultCollector = Quagga.ResultCollector.create({
+        capture: true, // keep track of the image producing this result
+        capacity: 20,  // maximum number of results to store
+        blacklist: [],   // list containing codes which should not be recorded
+        filter: function(codeResult) {
+            // only store results which match this constraint
+            // returns true/false
+            // e.g.: return codeResult.format === "ean_13";
+            return true;
+        }
+      });
+      Quagga.registerResultCollector(resultCollector)
+      Quagga.init({
+        inputStream : {
+        name : "Live",
+        type : "LiveStream",
+        target: document.querySelector('#videoElement'),
+          // Or '#yourElement' (optional)
+        },
+        decoder : {
+          readers : ["ean_reader",
+                        "ean_8_reader",
+                        "upc_reader",
+                        "upc_e_reader"]
+        },
+        debug: {
+          drawBoundingBox: true,
+          showFrequency: true,
+          drawScanline: true,
+          showPattern: true
+        }
+      },  function(err) {
+            if (err) {
+              this.error = err
+                return console.log(err);
+            }
+            Quagga.start();
+          });
+      Quagga.onDetected((res)=> {
+          // this.results.push({'code': res.codeResult.code, 'format': res.codeResult.format})
+        this.results = resultCollector.getResults().map(result => result.codeResult)
+      })
+    }
+  }
+</script>
--- src/index.vue
+++ src/index.vue
@@ -1,14 +1,21 @@
 <template>
-    <text class="title">Hello {{target}}</text>
+  <div class="wrapper">
+    <div>
+      <quagga></quagga>
+    </div>
+  </div>
 </template>
 
 <style>
+/*Coming Soon*/
 </style>
 
 <script>
+// import Nat from 'natjs'
+import Quagga from './components/quagga.vue'
   export default {
-    data: {
-      target: 'World'
+    components: {
+      'quagga': Quagga
     }
   }
 </script>
