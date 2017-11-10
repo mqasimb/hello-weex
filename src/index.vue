b100bb3c2b06c20db7ef322e9dbd7a6517725e7c
Qasim Bhatti <bqasim381@gmail.com>
2017-11-10 15:48:57 -0500

    add v-if /else based on device

--- src/index.vue
+++ src/index.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="wrapper">
+  <template v-if="device === 'Web'">
   <button @click="startScanner">Start Scanner</button>
   <button @click="stopScanner">Close Scanner</button>
     <div v-if="showScanner">
       <quagga></quagga>
     </div>
+  </template>
+  <template v-else>
+  <button @click="captureImage">Start Camera</button>
+  <!-- <button @click="scannertwo">Start Camera</button> -->
+  <button @click="preview">Preview Image</button>
+  </template>
   </div>
 </template>
 
@@ -13,20 +20,46 @@
 </style>
 
 <script>
-// import Nat from 'natjs'
+import Nat from 'natjs'
+// console.log(weex)
 var Quagga = require('../components/quagga.vue')
+// var scannernew = weex.requireModule('scan')
   export default {
     data: function() {
       return {
-        showScanner: false
+        showScanner: false,
+        device: weex.config.env.platform
       }
     },
     methods: {
+      // scanner: function() {
+      //   scannernew.scanCapture();
+      // },
+      // scannertwo: function() {
+      //   scannernew.scanBarcode();
+      // },
       startScanner: function() {
         this.showScanner = true
       },
       stopScanner: function() {
         this.showScanner = false
+      },
+      captureImage() {
+        Nat.camera.captureImage({}, (err, ret) => {
+          if (err) {
+            Nat.toast('[ERROR] ' + JSON.stringify(err))
+            return
+          }
+          Nat.toast(JSON.stringify(ret, null, 2))
+            this.path = ret.path
+          })
+      },
+      preview() {
+        if (!this.path) {
+          Nat.toast('Please take your photo first :)')
+          return
+        }
+        Nat.image.preview(this.path)
       }
     },
     components: {
